e7911973c71d6dd4c1a2d8522d1cbf75556e230d
Gustavo Castilião <gucastiliao@gmail.com>
2018-02-08 22:53:36 -0200

    tests: add user creation test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+
+    use DatabaseTransactions;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCreateUser()
+    {
+        User::create(
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt(123456)
+            ]
+        );
+
+        $this->assertDatabaseHas('users',
+            [
+                'email' => 'admin@admin.com'
+            ]
+        );
+    }
+}
